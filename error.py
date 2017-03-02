fbc2fdbca3b7c38ee31c089d0d0983204c5b4f0f
erocoar <fj.tiedemann@googlemail.com>
2017-03-02 12:47:22 +0100

    Add files via upload

--- error.py
+++ error.py
@@ -24,19 +24,19 @@ def errorcheck(number_dimensions, voters, status_quo, alter_status_quo, drift_st
         if preference_shape == 'circle':
             # check if all voters have the correct length
             if number_dimensions != len(voter.position):
-                print 'Voters do not seem to have the correct number of dimensions. The program will now exit.'
+                print ('Voters do not seem to have the correct number of dimensions. The program will now exit.')
                 errorcheck_passed = False
 
         if preference_shape == 'ellipse':
             if number_dimensions != 2:
-                print 'Elliptical preferences can only have two dimensions. The program will now exit.'
+                print ('Elliptical preferences can only have two dimensions. The program will now exit.')
             if len(voter.position) != 5:
-                print 'Elliptical preferences input incorrect (values may be missing). The program will now exit.'
+                print ('Elliptical preferences input incorrect (values may be missing). The program will now exit.')
 
         # check if there is only one agenda setter
         if voter.agenda_setter == True:
             if agenda_setter_found == True:
-                print 'You have entered multiple agenda setters. The program will now exit.'
+                print ('You have entered multiple agenda setters. The program will now exit.')
                 errorcheck_passed = False
             else:
                 agenda_setter_found = True
@@ -44,18 +44,18 @@ def errorcheck(number_dimensions, voters, status_quo, alter_status_quo, drift_st
     # check if the status quo has a drift of correct length
     if alter_status_quo == 'history and drift':
         if len(drift_status_quo) != number_dimensions:
-            print 'The drift of your status quo does not have the correct number of dimensions. The program will now exit.'
+            print ('The drift of your status quo does not have the correct number of dimensions. The program will now exit.')
             errorcheck_passed = False
 
     # check if the players have a drift of correct length
     if alter_status_quo == 'history and drift':
         if len(drift_players) != number_dimensions:
-            print 'The drift of your players does not have the correct number of dimensions. The program will now exit.'
+            print ('The drift of your players does not have the correct number of dimensions. The program will now exit.')
             errorcheck_passed = False
 
     # check if the user attempts to create a visualization of a non-two-dimensional simulation
     if visualize == True and number_dimensions != 2:
-        print 'Visualizations are only possible in two-dimensional simulations. The program will now exit.'
+        print ('Visualizations are only possible in two-dimensional simulations. The program will now exit.')
         errorcheck_passed = False
 
     return errorcheck_passed
